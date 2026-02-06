449536adc5348bbe8deaace790aed21d8c5f6b21
Ovett Mora <ovettmora17@gmail.com>
2026-02-05 23:43:14 -0600

    feat: add Jira icon component and update technologies data structure

--- src/components/TechnologiesSection/TechnologiesCarousel.tsx
+++ src/components/TechnologiesSection/TechnologiesCarousel.tsx
@@ -1,13 +1,18 @@
-import { technologies } from "@/data/technologies";
-import "./TechnologiesCarousel.css";
+import type { Technologie } from '@/types/Technologie';
+import './TechnologiesCarousel.css';
 
-const TechnologiesCarousel = () => {
+interface Props {
+  technologies: Technologie[];
+}
+
+const TechnologiesCarousel = ({ technologies }: Props) => {
   const iconsLoop = [...technologies, ...technologies];
+
   return (
     <div className="slider">
       <div className="slide-track">
         {iconsLoop.map(({ name, icon: Icon }, idx) => (
-          <div key={`icon-${idx}`} className="slide flex flex-col items-center">
+          <div key={`icon-${name}-${idx}`} className="slide flex flex-col items-center">
             <Icon className="w-12 h-12" />
             <p className="text-center mt-2 text-sm">{name}</p>
           </div>
